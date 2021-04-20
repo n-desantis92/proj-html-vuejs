776ef9e0687eacc632925c65e9bb11d4535358c7
n-desantis92 <dsnwebdev@gmail.com>
2021-04-20 17:53:27 +0200

    aggiunte a main3 v-for, img  e titoli

--- src/app.js
+++ src/app.js
@@ -37,6 +37,38 @@ var app = new Vue({
         info: "All Wplms modules in App. Conferencing, Calendars.",
       }
     ],
+    listaCorsi: [
+      {
+        corsImg: "course4-600x359.png",
+        corsTitle: "Software training reloaded",
+        settore: "INFORMATION TECH / MACHINE LEARNING / TECHNOLOGY",
+      },
+      {
+        corsImg: "course3-600x359.png",
+        corsTitle: "Data mining",
+        settore: "DATA",
+      },
+      {
+        corsImg: "course2-1-600x359.png",
+        corsTitle: "Design Dynamics",
+        settore: "DESIGN / INFORMATION TECH",
+      },
+      {
+        corsImg: "course6-600x359.png",
+        corsTitle: "Understanding Color Psychology",
+        settore: "DESIGN",
+      },
+      {
+        corsImg: "course7-600x359.png",
+        corsTitle: "Impact of a minimalist Design",
+        settore: "DESIGN",
+      },
+      {
+        corsImg: "course1-600x359.png",
+        corsTitle: "Machine Learning Tools",
+        settore: "INTELLIGENCE / TECHNOLOGY",
+      },
+    ],
   },
   method: {
 
